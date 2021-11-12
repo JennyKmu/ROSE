fe7eb58107da78590857471b5ce747888751a141
Leif-The-Head <93671207+Leif-The-Head@users.noreply.github.com>
2021-11-12 14:51:51 +0100

    Feature: Edit Industries

--- RRO_savefile_editor/UI/industryPlacables.py
+++ RRO_savefile_editor/UI/industryPlacables.py
@@ -1,35 +1,36 @@
 industryNames = {
-    2: "Logging Camp",
-    3: "Sawmill",
-    4: "Iron Mine",
-    5: "Smelter",
-    6: "Coal Mine",
-    7: "Ironworks",
-    8: "Oilfield",
-    9: "Refinery",
+    1: "Logging Camp",
+    2: "Sawmill",
+    8: "Iron Mine",
+    3: "Smelter",
+    7: "Coal Mine",
+    4: "Ironworks",
+    5: "Oilfield",
+    6: "Refinery",
 }
 
 
 industryInputs = {
-    3: {"log": 100},
-    4: {"beam": 20, "lumber": 24},
-    5: {"cordwood": 100, "ironore": 1000},
-    6: {"beam": 20, "rail": 100},
-    7: {"coal": 1000, "rawiron": 100},
-    8: {"beam": 20, "steelpipe": 100, "crate_tools": 100},
-    9: {"lumber": 20, "steelpipe": 100, "crudeoil": 1000},
+    1: [[None], [None], [None], [None]],
+    2: [["log", 100], [None], [None], [None]],
+    8: [["lumber", 24], ["beam", 20], [None], [None]],
+    3: [["cordwood", 100], ["ironore", 1000], [None], [None]],
+    7: [["beam", 20], ["rail", 50], [None], [None]],
+    4: [["rawiron", 100], ["coal", 1000], [None], [None]],
+    5: [["steelpipe", 18], ["beam", 20], ["crate_tools", 100], [None]],
+    6: [["crudeoil", 1000], ["steelpipe", 100], ["lumber", 24], [None]],
 }
 
 
 industryOutputs = {
-    2: {"log": 100, "cordwood": 100},
-    3: {"beam": 100, "lumber": 100},
-    4: {"ironore": 290},
-    5: {"rail": 100, "rawiron": 100},
-    6: {"coal": 750},
-    7: {"steelpipe": 100, "crate_tools": 100},
-    8: {"crudeoil": 1000},
-    9: {"oilbarrel": 100, "oilbarrel2": 100},
+    1: [["log", 100], ["cordwood", 32], ["cordwood", 32], ["log", 100]],
+    2: [["lumber", 100], ["beam", 100], [None], [None]],
+    8: [["ironore", 290], [None], [None], [None]],
+    3: [["rawiron", 100], ["rail", 100], [None], [None]],
+    7: [["coal", 750], [None], [None], [None]],
+    4: [["steelpipe", 100], ["crate_tools", 100], [None], [None]],
+    5: [["crudeoil", 1000], [None], [None], [None]],
+    6: [["oilbarrel", 100], ["oilbarrel", 100], [None], [None]],
 }
 
 
